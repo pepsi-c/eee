78c7f101e30a800328907623cbe42ec52b42425e
pepsi-c <69380680+pepsi-c@users.noreply.github.com>
2021-01-06 21:47:15 -0700

    Create howhorny.js

---
+++ commands/fun/howhorny.js
@@ -0,0 +1,22 @@
+const discord = require('discord.js');
+
+module.exports = {
+        name: "howgay",
+        aliases: ["gaymeter"],
+        category: "Fun",
+        description: "Shows how gay someone is.",
+        usage: "$howhorny <mention>",
+    run: async (bot, message, args) => {
+
+        let target = message.guild.member(message.mentions.users.first());
+        function between(min, max) {  
+            return Math.floor(
+              Math.random() * (max - min) + min
+            )
+          }
+    
+        if(!target) message.channel.send(`You are ` + between(1, 100) + '% horny :flushed: ')
+        else { message.channel.send(`<@${target.id}>` + ' is ' + between(0, 100) + '% horny :flushed: '); }
+    }
+
+};
